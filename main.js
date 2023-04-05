7f389cdac94ef89e283592e6baf5e26a326ff742
anaprr <129889860+anaprr@users.noreply.github.com>
2023-04-05 12:19:01 -0300

    alteração somas dois

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 document.addEventListener("click" , showcolor );
 
-document.getElementById("botao").addEventListener("click", showname);
+document.getElementById("botao").addEventListener("click", exibenome);
 
+document.getElementById("btn-soma").addEventListener("click", exiberesultado);
 
 function showcolor()
 {
     document.body.style.backgroundColor = "pink";
 }
-function showname()
+function exibenome()
 {
     let usuario= document.getElementById("nome").value;
     document.getElementById("mensagem").innerHTML="Meu nome é" + usuario;
     document.getElementById("nome").value="";
+}
+function exiberesultado()
+{
+    let numero1= document.getElementById("num1").value;
+    let numero2= document.getElementById("num2").value;
+    document.getElementById("resultado1").innerHTML=parseFloat( numero1) + parseFloat(numero2);
 }
